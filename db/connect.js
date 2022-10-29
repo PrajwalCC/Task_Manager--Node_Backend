9d3d7c4cc1b66404fa84cfa81f278ce80bfffbd9
PrajwalCC <b220035@iiit-bh.ac.in>
2022-10-29 12:18:33 +0530

    updating db connection

--- db/connect.js
+++ db/connect.js
@@ -1,38 +1,35 @@
 const mongoose = require('mongoose');
+
 const { connect, connection } = require('mongoose');
-
-
-
-
-// const url = process.env.MONGO_URI
-// const connectDB = ()=>{
-//     return mongoose.connect(url,{
-//         useNewUrlParser: true,
-//         useCreateIndex: true,
-//         useFindAndModify: false,
-//         useUnifiedTopology: true,
-//     })
-// }
-
 const connectDB = _ => {
-    const mongoURI = process.env.MONGO_URI
-    try {
-      connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+  const mongoURI = process.env.MONGO_URI
+  try {
+    connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     } catch (err) {
       console.error('Initial Database Connection Error!', err);
     }
-  
+    
     connection.on('connected', _ =>
-      console.log('Database connected ==> ', mongoURI)
+    console.log('Database connected ==> ', mongoURI)
     );
-  
+    
     connection.on('error', err =>
-      console.error('Database Connection Error!\n', err)
+    console.error('Database Connection Error!\n', err)
     );
   
   };
 
+    // const mongoURI = process.env.MONGO_URI
+    // const mongoURI = 'mongodb+srv://Prajwal:Prajwal@123@taskmanager.qf7ewkg.mongodb.net/?retryWrites=true&w=majority'
+    // const connectDB = (url)=>{
+    //   return mongoose.connect(mongoURI,{
+    //     useNewUrlParser: true,
+    //     useCreateIndex: true,
+    //     useFindAndModify: false,
+    //     useUnifiedTopology: true,
+    //   })
+    // }
 module.exports = connectDB
